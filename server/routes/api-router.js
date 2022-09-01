b248ad576a6849433f8cf85f83255627305cfc32
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-09-01 17:59:14 -0400

    UPDATE minor API fix

--- server/routes/api-router.js
+++ server/routes/api-router.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProject, getAllProjects, getProjectsPage } = require('../controllers/project')
+const { getProject,
+    getAllProjects,
+    getProjectsPage,
+    getCount
+} = require('../controllers/project')
 
 router.route('/').get(getAllProjects)
 router.route('/:page').get(getAllProjects)
 router.route('/project/:pid').get(getProject)
 router.route('/category/:filter').get(getProjectsPage)
 router.route('/category/:filter/:page').get(getProjectsPage)
+router.route('/count/count').get(getCount)
 
 module.exports = router
