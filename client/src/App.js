28ec31f2d17ee26c8f33f06dd6c2978a72ba0617
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-09-01 18:58:55 -0400

    ADD ProjectBox component
    + ProjectBox starter component
    + Cleaned up boilerplate code in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import ProjectBox from './components/ProjectBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProjectBox pID="63112c7dfd3ccec5b3092832"/>
     </div>
   );
 }
