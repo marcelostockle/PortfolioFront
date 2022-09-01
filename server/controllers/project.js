0d8332fdc505f6d81957c828951a854246a6faa6
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-09-01 16:22:06 -0400

    UPDATE server API update
    + Pagination
    + Tag filters

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -1,15 +1,29 @@
 const Project = require('../models/project')
 
+const limit = 12
 const getProject = async (req, res) => {
-    query_result = await Project.findById(req.params.pid)
+    query_result = await Project.findById(req.params.pid).exec()
     res.status(200).json(query_result)
 }
 const getAllProjects = async (req, res) => {
-    query_result = await Project.find()
+    const skip = req.params.page ? req.params.page * limit : 0
+    query_result = await Project.find({}, null, {limit, skip}).exec()
+    res.status(200).json(query_result)
+}
+const getCount = async (req, res) => {
+    query_result = await Project.estimatedDocumentCount().exec()
+    res.status(200).json({count: query_result})
+}
+const getProjectsPage = async (req, res) => {
+    const skip = req.params.page ? req.params.page * limit : 0
+    const queryFilter = req.params.filter
+    query_result = await Project.find({tags: queryFilter}, null, {limit, skip}).exec()
     res.status(200).json(query_result)
 }
 
 module.exports = {
     getProject,
-    getAllProjects
+    getAllProjects,
+    getProjectsPage,
+    getCount
 }
--- server/routes/api-router.js
+++ server/routes/api-router.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProject, getAllProjects } = require('../controllers/project')
+const { getProject, getAllProjects, getProjectsPage } = require('../controllers/project')
 
 router.route('/').get(getAllProjects)
-router.route('/:pid').get(getProject)
+router.route('/:page').get(getAllProjects)
+router.route('/project/:pid').get(getProject)
+router.route('/category/:filter').get(getProjectsPage)
+router.route('/category/:filter/:page').get(getProjectsPage)
 
 module.exports = router
