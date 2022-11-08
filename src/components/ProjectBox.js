d022359baa835cbed34c03f17fc65e02b7e498eb
marcelostockle <marcelostockle@hotmail.com>
2022-11-08 20:11:15 -0300

    Minor style changes

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -2,23 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 import { AiFillGithub } from "react-icons/ai";
 
-const TopBox = styled.div`
+const TopBox = styled.a`
 min-width: 300px;
 width: 35vw;
+height: fit-content;
 background-color: navajowhite;
-border-width: 3px;
+border-width: 2px;
 border-style: outset;
+border-color: beige;
 display: grid;
 justify-items: center;
 padding: 0.5rem 0.2rem;
 margin: 1.5rem 1.5rem;
+text-decoration: none;
+color: black;
 `
-const Title = styled.a`
+const Title = styled.h2`
 width: fit-content;
+height: fit-content;
 background-color: aliceblue;
 padding: 0.2rem 0.4rem;
 margin-top: 0.5rem;
-text-decoration: none;
 font-weight: 300;
 font-size: larger;
 color: black;
@@ -40,12 +44,17 @@ padding: 0.5rem;
 `
 
 const ProjectBox = (project) => {
+    if (project.repository) {
+
+    }
     return (
-        <TopBox>
+        <TopBox target="_blank" href={project.link}>
             <Thumbnail src={project.thumbnail} alt={project.title}/>
-            <Title target="_blank" href={project.link}>{project.title}</Title>
+            <Title>{project.title}</Title>
             <CleanParagraph>{project.description}</CleanParagraph>
-            <a target="_blank" href={project.repository}><GithubIcon size={36}/></a>
+            { project.repository &&
+                <a target="_blank" href={project.repository}><GithubIcon size={36}/></a>
+            }
         </TopBox>
     )
 }
