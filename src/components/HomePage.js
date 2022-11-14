276acd2ae5c9d1afd8ac78d15fe9997a349e08c4
marcelostockle <marcelostockle@hotmail.com>
2022-11-13 22:47:12 -0300

    Added HomePage content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import { AiOutlineDownload } from 'react-icons/ai'
 
 const TopContainer = styled.div`
   width: 500px;
   margin: 10rem 80px;
   display: grid;
   justify-content: flex-start;
+  color: white;
 `
 const Tagline = styled.div`
   width: fit-content;
@@ -13,17 +15,32 @@ const Tagline = styled.div`
   border-color: beige;
   border-style: outset;
   border-width: thin;
-  color: white;
 `
-const Title = styled.h1`
-  color: white;
+const DownloadLink = styled.a`
+  width: fit-content;
+  margin: 1rem 3rem;
+  padding: 0.2rem 0.5rem;
+  border-color: beige;
+  border-style: outset;
+  border-width: thin;
+  background-color: bisque;
+  color: black;
+  text-decoration: none;
+  font-weight: 300;
+  font-size: larger;
+  display: flex;
+  align-items: center;
 `
+
 const HomePage = () => {
   return (
     <TopContainer>
       <Tagline>Freelance Web Developer</Tagline>
-      <Title>Hi! I'm Marcelo Stöckle</Title>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1>Hi! I'm Marcelo Stöckle</h1>
+      <p>Informatics Engineer. Bachellor of Biochemistry. Job aspirations: full-stack web development (node, ExpressJS, ReactJS, Java Spring), QA-automation, DevOps. Experience in development of scientific applications with Java, Matlab and Python/Jupyter. Fluent English and Spanish, conversational Japanese, basic German, Dutch and French</p>
+      <DownloadLink href="/Curriculum-MarceloStockle-eng.pdf" download>
+        <AiOutlineDownload/> Download CV
+      </DownloadLink>
     </TopContainer>
   )
 }
