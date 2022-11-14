e32d293d4716e83973543dd5e59976f8b471efa5
marcelostockle <marcelostockle@hotmail.com>
2022-11-13 23:21:20 -0300

    Additional referral links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import { AiOutlineLinkedin, AiFillGithub } from 'react-icons/ai'
 
 const Nav = styled.nav`
 background: aliceblue;
@@ -21,6 +22,16 @@ cursor: pointer;
 &.active {
   color: #4d4dff;
 }`
+const RoundedIcon = styled.a`
+  width: fit-content;
+  height: fit-content;
+  border-style: solid;
+  border-width: thin;
+  border-radius: 10px;
+  color: #808080;
+  padding: 0.3rem 0.3rem;
+  margin-left: 0.3rem;
+`
 
 const Navbar = () => {
 
@@ -28,6 +39,8 @@ const Navbar = () => {
     <StyledLink to="/">Home</StyledLink>
     <StyledLink to="/projects">Projects</StyledLink>
     <StyledLink to="/about">About</StyledLink>
+    <RoundedIcon href="https://www.linkedin.com/in/marcelo-st%C3%B6ckle-113479237/"><AiOutlineLinkedin size={30}/></RoundedIcon>
+    <RoundedIcon href="https://github.com/marcelostockle"><AiFillGithub size={30}/></RoundedIcon>
   </Nav>)
 }
 
